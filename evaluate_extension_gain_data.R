122e2c9329ee8e61613412beff57e98a2ade3573
Christian Wachauf <christianwachauf@Palmer-Eldritch.dhcp.e14.ph.tum.de>
2016-10-04 19:15:47 +0200

    some minor additions..

--- evaluate_extension_gain_data.R
+++ evaluate_extension_gain_data.R
@@ -1,6 +1,6 @@
 source("polym_mechanics.R")
 
-Load_Extension_Gain_Data <- function(index)
+Load_Extension_Gain_Data <- function(index,num_iters)
 {
   full_path <- "/Users/christianwachauf/Documents/Daten/data_stack_faki/Ext_Gains_vs_Force/"
   filename <- paste0("ext_gain_A0",toString(index),".txt")
@@ -9,9 +9,13 @@ Load_Extension_Gain_Data <- function(index)
   head(df_ext_gain_data)
   
   plot(df_ext_gain_data[,3],df_ext_gain_data[,4],xlim=c(0,8),ylim=c(0,11),xlab="force [pN]",ylab="extension gain [nm]")
-  df_ext_gain_theor <- Plot_Extension_Gain_From_Force_Open(L=40,P=0.88,Temp=298.15,dz=3.5,F_min=2,F_max=6,npnts=200)
-  points(df_ext_gain_theor$forces_open,df_ext_gain_theor$ext_gains_open,type="l")
-  sf_ext_gain_data <- Stan_Fit_Extension_Gain_Data(forces_open=df_ext_gain_data[,3],extension_gains = df_ext_gain_data[,4])
+##  df_ext_gain_theor <- Plot_Extension_Gain_From_Force_Open(L=40,P=0.88,Temp=298.15,dz=3.5,F_min=2,F_max=6,npnts=200)
+ ## points(df_ext_gain_theor$forces_open,df_ext_gain_theor$ext_gains_open,type="l")
+  sf_ext_gain_data <- Stan_Fit_Extension_Gain_Data(forces_open=df_ext_gain_data[,3],extension_gains = df_ext_gain_data[,4],num_iters=num_iters)
+  df_prediction_bands <- Plot_Stan_Extension_Gain_From_Force_Open(sf_ext_gain_data,F_min=2,F_max=7,npnts=100,nsamples=num_iters/2)
+  points(df_prediction_bands$forces,df_prediction_bands$ext_gain_mean,type="l")
+  points(df_prediction_bands$forces,df_prediction_bands$ext_gain_lp,type="l",lty=2)
+  points(df_prediction_bands$forces,df_prediction_bands$ext_gain_up,type="l",lty=2)
   return(sf_ext_gain_data)
 }
 
@@ -22,6 +26,46 @@ Stan_Fit_Extension_Gain_Data <- function(forces_open,extension_gains,num_iters=1
   stan_init <- list(list(P=1.0,CL=40.0,sigma=0.5))
   stan_fit_ext_gain_data <- stan(file="/Users/christianwachauf/Documents/Scripts/GithubRepo/B_OT_Data_Analysis/Stan/fit_ext_gain.stan",data=stan_data,init=stan_init,iter=num_iters,chain=1)
   return(stan_fit_ext_gain_data)
+}
+
+Plot_Stan_Extension_Gain_From_Force_Open <- function(stan_fit_ext_gain_data,F_min=2,F_max=7,npnts,nsamples)
+{
+  require("coda")
+  mat <- as.matrix(stan_fit_ext_gain_data)
+  
+  forces <- seq(from=F_min,to=F_max,length=npnts)
+  
+  ext_gain_mean <- array(0,dim=c(npnts))
+  ext_gain_lp <- array(0,dim=c(npnts))
+  ext_gain_up <- array(0,dim=c(npnts))
+  
+  predictions_ext_gain <- array(0,dim=c(nsamples,npnts))
+  
+  for(i in 1:nsamples)
+  {
+    curr_P <- mat[i,1]
+    curr_CL <- mat[i,2]
+    curr_sigma <- mat[i,3]
+    curr_Temp <- mat[i,4]
+    curr_dz <- mat[i,5]
+    curr_k_eff <- mat[i,6]
+    
+    
+    for(j in 1:npnts)
+    {
+      predictions_ext_gain[i,j] <- Extension_Gain_From_Force_Open(forces[j],curr_CL,curr_P,curr_Temp,curr_dz)
+      predictions_ext_gain[i,j] <- predictions_ext_gain[i,j] + rnorm(n=1,mean=0,sd=curr_sigma)
+    }
+  }
+  for(j in 1:npnts)
+  {
+    ext_gain_mean[j] <- mean(predictions_ext_gain[,j])
+    hdi_values <- HPDinterval(as.mcmc(predictions_ext_gain[,j]))
+    ext_gain_lp[j] <- hdi_values[1];
+    ext_gain_up[j] <- hdi_values[2];
+  }
+  df_preds <- data.frame(forces,ext_gain_mean,ext_gain_lp,ext_gain_up)
+  return(df_preds)
 }
 
 Extension_Gain_From_Force_Open <- function(F_open,L,P,Temp=298.15,dz=3.5)
